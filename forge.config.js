376cd1c40b312437ba67240cbe823c42079c86ab
Jules CROIZIER <99172254+roulios44@users.noreply.github.com>
2025-09-29 10:10:42 +0200

    fix: forge config

--- forge.config.js
+++ forge.config.js
@@ -68,8 +68,8 @@ module.exports = {
       name: "@electron-forge/publisher-github",
       config: {
         repository: {
-          owner: "mon-compte",
-          name: "mon-repo",
+          owner: "roulios44",
+          name: "ElectronJSDemo",
         },
         prerelease: false,
         draft: true,
